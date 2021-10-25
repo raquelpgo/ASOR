73b2bbf2ffeb6f63cb7f9dd1b92df7e33f24986e
raquelpgo <76945707+raquelpgo@users.noreply.github.com>
2021-10-25 20:55:09 +0200

    Update ejercicio5.c

--- practica2.1/ejercicio5.c
+++ practica2.1/ejercicio5.c
@@ -4,10 +4,15 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <string.h>
+#include <errno.h>
 
 int main() {
 	struct utsname buf;
-	uname(&buf);
+	if (uname(&buf) != 0)
+	{
+		printf("ERROR %d - %s", errno, strerror(errno));
+		return -1;
+	}
 	printf("%s, %s, %s, %s, %s\n", buf.sysname, buf.nodename, buf.release, 
 		buf.version, buf.machine);
 	return 0;	
