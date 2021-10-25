c596f02b82fac7807051c767656fac392ac495b4
raquelpgo <76945707+raquelpgo@users.noreply.github.com>
2021-10-25 21:49:09 +0200

    Create ejercicio8.c

---
+++ practica2.1/ejercicio8.c
@@ -0,0 +1,13 @@
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+
+
+int main() 
+{
+	printf("UID: %d\n",getuid());
+	return 0;
+}
