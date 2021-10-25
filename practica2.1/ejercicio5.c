3b534502a872e9008166652037b10167d2c9494d
raquelpgo <76945707+raquelpgo@users.noreply.github.com>
2021-10-25 20:51:51 +0200

    Update ejercicio5.c

--- practica2.1/ejercicio5.c
+++ practica2.1/ejercicio5.c
@@ -1,1 +1,14 @@
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 
+int main() {
+	struct utsname buf;
+	uname(&buf);
+	printf("%s, %s, %s, %s, %s\n", buf.sysname, buf.nodename, buf.release, 
+		buf.version, buf.machine);
+	return 0;	
+}
