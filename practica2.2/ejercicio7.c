b0f41a25fbe3d5d6b82a7e54e46435d813e85d48
raquelpgo <76945707+raquelpgo@users.noreply.github.com>
2021-11-02 18:39:45 +0100

    Create ejercicio7.c

---
+++ practica2.2/ejercicio7.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2) {
+		printf("Especifique la ruta del fichero!\n");
+		return -1;	
+	}
+
+	mode_t valor_anterior = umask(027); 
+//para el usuario todos los permisos 7-7=0, para el grupo rx 7-5=2 y para otros ninguno 7-0=7
+
+	if (open(argv[1], O_CREAT | O_TRUNC, 0777) == -1) 
+	{
+		printf("ERROR %d - %s\n", errno, strerror(errno));
+		return -1;
+	}
+	printf("Anterior: %d\n", valor_anterior);
+
+	return 0;
+
+}
