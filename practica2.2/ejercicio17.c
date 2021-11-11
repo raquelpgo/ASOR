2f843931dbf7e28c0e6993e427e36568769a2a8d
raquelpgo <76945707+raquelpgo@users.noreply.github.com>
2021-11-11 23:24:30 +0100

    Create ejercicio17.c

---
+++ practica2.2/ejercicio17.c
@@ -0,0 +1,85 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+
+
+	if (argc < 2) 
+	{
+		 printf("Especifique la ruta del fichero!\n");
+		 return -1;
+	}
+
+	DIR *dir = opendir(argv[1]);
+
+	if (dir == NULL )
+	{
+		printf("No existe el directorio!\n");
+		return -1;
+	}
+
+	struct dirent *act;
+	struct stat info;
+	size_t spath = strlen(argv[1]);
+	act = readdir(dir);
+	long int total = 0;
+
+	while (act != NULL) 
+	{
+    		char *path = malloc(sizeof(char)*(spath + strlen(act->d_name) + 3));
+		strcpy(path, argv[1]);
+    		strcat(path, "/");
+    		strcat(path, act->d_name);
+
+		if (stat(path, &info) == -1) 
+		{
+      			printf("Ha habido un problema al leer el archivo!\n");
+      			free(path);
+      			closedir(dir);
+      			return -1;
+    		} 
+	
+		else 
+		{
+      			if (S_ISREG(info.st_mode))
+			{
+				
+        			printf("%s", act->d_name);
+				if ((info.st_mode == S_IXUSR) || (info.st_mode == S_IXGRP) || (info.st_mode == S_IXOTH))
+				{
+					printf(" *");				
+				}
+				printf("\n");
+        			total +=  (info.st_blksize/8)*info.st_blocks;
+
+      			}
+	 
+			else if (S_ISDIR(info.st_mode)) 
+			{
+        			printf("%s/ \n", act->d_name);
+
+			} 
+
+			else if (S_ISLNK(info.st_mode)) 
+			{
+       	 			char *nenlace = malloc(info.st_size + 1);
+        			readlink(path, nenlace, info.st_size + 1);
+        			printf("%s->%s \n", act->d_name, nenlace);
+        			free(nenlace);
+	     		}
+    		}
+
+    		free(path);
+    		act = readdir(dir);
+  	}
+
+  	closedir(dir);
+  	return 0;
+}
