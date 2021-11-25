f4723b4ff7d470ae8aa94de9b837dbc9a1fad012
raquelpgo <76945707+raquelpgo@users.noreply.github.com>
2021-11-25 23:18:48 +0100

    Update ejercicio11.c

--- practica2.3/ejercicio11.c
+++ practica2.3/ejercicio11.c
@@ -1,35 +1,54 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 
 int main(int argc, char **argv)
 {
-	sigset_t set;
-	sigemptyset(&set);
-	sigaddset(&set, SIGINT);
-	sigaddset(&set, SIGTSTP);
-	sigprocmask(SIG_BLOCK, &set, NULL);
+
+	sigset_t masc;
+
+	sigemptyset(&masc);
+	sigaddset(&masc, SIGINT);
+	sigaddset(&masc, SIGTSTP);
+
+	if (sigprocmask(SIG_BLOCK, &masc, NULL) == -1)
+	{
+		printf("ERROR %i - %s \n", errno, strerror(errno));
+		exit(-1);
+	}
 
 	char *sleep_secs = getenv("SLEEP_SECS");
+
+	if (sleep_secs == NULL)
+	{
+		printf("ERROR %i - %s \n", errno, strerror(errno));
+		exit(-1);
+	}
 	int secs = atoi(sleep_secs);
 	sleep(secs);
 
 	sigset_t pending;
-	sigpending(&pending);
+	if (sigpending(&pending) == -1) 
+	{
+		printf("ERROR %i - %s \n", errno, strerror(errno));
+		exit(-1);
+	}
 
 	if (sigismember(&pending, SIGINT) == 1) 
 	{
 		printf("SIGINT\n");
-		sigdelset(&set, SIGINT);
+		sigdelset(&masc, SIGINT);
 	} 
 
 	if (sigismember(&pending, SIGTSTP) == 1) 
 	{
 		printf("SIGTSTP\n");
-		sigdelset(&set, SIGTSTP);
+		sigdelset(&masc, SIGTSTP);
 	} 
-	sigprocmask(SIG_UNBLOCK, &set, NULL);
+	sigprocmask(SIG_UNBLOCK, &masc, NULL);
 
 	return 0;
 }
